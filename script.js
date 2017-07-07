e8231164d3330a630256c5ff6e853e6db80d6826
Joel Gramling <joelgramling@Joels-MBP.fios-router.home>
2017-07-07 12:58:07 -0400

    Added 2 menubars (top and side) cleaned up body of webpage.

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 $(window).load(function(){
-    $('.main, .menu, .info').removeClass('hidden').addClass('fadeInUp animate');
+    $('.main, .info').removeClass('hidden').addClass('fadeInUp animate');
     $('.loaderOverlay').css("display", "none");
 });
 
 $(document).ready(function(){
+  executeParallax();
+});
+
+function executeParallax() {
   var i = 0;
   var slides = ["#info1", "#info2", "#info3", "#info4"];
+  var menuBarSide = $('.menu');
+  var menuBarTop = $('.menuTop');
     $(window).scroll(function(){
+      console.log(!menuBarSide.hasClass('hidden'));
       var topY = $(window).scrollTop();
         $('.threeD').each(function(){
           var $layer = $(this);
 			    var yPos = -(topY * $layer.attr('speed') / 100);
-			      $layer.css("transform", "translate(0px, " + yPos + "px)");
+			      $layer.css("transform", "translate3d(0px, " + yPos + "px, 0px)");
         });
-        if(i<4 && topY >= $(slides[i]).position().top + 500){
-          console.log(topY, $(window).height());
-          console.log(slides, i, $(slides[i]).position());
+       if (topY > menuBarTop.height() && $('.loaderOverlay').css("display") === "none"){
+          menuBarSide.removeClass('hidden fadeOutUp').addClass('fadeInRight');
+          } else if (topY < menuBarTop.height() && $('.loaderOverlay').css("display") === "none" && !menuBarSide.hasClass('hidden')){
+          menuBarSide.removeClass('hidden fadeInRight').addClass('fadeOutUp');
+        }
+        if(i<4 && topY >= $(slides[i]).position().top + 700){
           //var direction = $(this).attr('direction')
           $(slides[i]).removeClass('hidden').addClass('fadeInUp animate');
           i++;
@@ -24,5 +34,4 @@ $(document).ready(function(){
     //$('.menuItem').hover(function(){
       //$(this).css("transform", "translate(20px, 0)");
     //});
-});
-
+}
